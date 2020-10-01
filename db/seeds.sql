d7fae6413d2ef4186c1e8e44601b7809b3bbea1d
Gshow <chukwuanumba@gmail.com>
2020-10-01 09:48:13 -0700

    setups

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name,devoured) VALUES ('Cheese burger', true);
+INSERT INTO burger (burger_name,devoured) VALUES ('veggie burger', false);
+INSERT INTO burger (burger_name,devoured) VALUES ('salmon burger', false);
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
